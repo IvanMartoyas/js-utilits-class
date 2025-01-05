9d2e76a6f72db3a6ada8ada51ba476cc6205c96f
Ivan Martoyas <51325447+IvanMartoyas@users.noreply.github.com>
2025-01-05 23:37:24 +0300

    Добавил загрузку base64 из файла

--- utilits.js
+++ utilits.js
@@ -46,7 +46,7 @@ class Utilits {
     array_Find_By_Id(array, serach_id) {
         try {
 
-            return this.array.filter(item => item.id == delete_id);
+            return array.filter(item => item.id == serach_id);
             
         } catch (e){ 
             if(this.debug)  console.log(" array_Find_By_Id function error: ", e)
@@ -57,7 +57,7 @@ class Utilits {
         try {
 
             // вернёт массив уже без удалённого элемента
-            let finding = this.array.filter(item => item.id !== delete_id);
+            let finding = array.filter(item => item.id !== delete_id);
             return finding;
 
         } catch (e){ 
@@ -65,15 +65,63 @@ class Utilits {
         }
     }
 
+    // рандомизация начало
     getRandomNumber(min, max) { 
         // возвращает сгенерированное число с заданным диапозоном
         return Math.round( Math.random() * (max - min) + min);
     }
 
+    getRandomIdSeed() {
+        // на основе времени создаёт рандомное значение 
+        let time = new Date();
+        time = time.getTime();
+        return time;
+    }
+    // рандомизация конец
+
     convert_color_to_alpha(color, opacity) { 
         // переводит обычный цвет без opacyty в rgba с прозрачностью
         var _opacity = Math.round(Math.min(Math.max(opacity ?? 1, 0), 1) * 255);
         return color + _opacity.toString(16).toUpperCase();
+    }
+
+    // кодирует изображение из file в base64
+    toBase64InFile(_input_file_id, prevuew_img) {
+        // _input_file_id  принимает id input file c которого достаю изображение
+        // prevuew_img (не обязательный) если нужно сделать превью после загрузки то указываю id c img куда отобразиться картнка
+
+        document.getElementById(_input_file_id).onchange = event => {
+            let reader = new FileReader();
+
+            if(prevuew_img) {
+                reader.onload = e => document.getElementById(prevuew_img).src = e.target.result;
+            }
+
+            reader.readAsDataURL(event.target.files[0]);
+            console.log("render ",reader)
+        }
+    }
+
+    createStyleTagInHead(css_code, priority) {
+        if(priority == undefined || css_code == '') {
+            priority = 0;
+        }
+        if(css_code == undefined || css_code == '') {
+            console.info("createStyleTagInHead not data in css code");
+            return
+        }
+        // позволяет вставить в header сгенерированный css в созданном теге style 
+        // css_code  строка с сгенерированным css
+        // priority приоритет вставки в числовом значении
+
+        let Style = document.createElement('style');
+
+        if (Style.styleSheet) {
+            Style.styleSheet.cssText = css_code;
+        } else {
+            Style.appendChild(document.createTextNode(css_code));
+        }
+        document.getElementsByTagName('head')[priority].appendChild(Style);
     }
 }
 let utilits = new Utilits()
